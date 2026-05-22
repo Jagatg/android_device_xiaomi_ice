637f4aa44d85e6db0c1f45acf5367e3fa5e1a26b
jagath <jjjagath9@gmail.com>
2026-05-22 14:12:50 +0530

    ice: add some flags in devices.mk

--- device.mk
+++ device.mk
@@ -10,3 +10,35 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+AB_OTA_UPDATER := true
+
+# A/B support
+PRODUCT_PACKAGES += \
+    com.android.hardware.boot \
+    android.hardware.boot-service.default_recovery
+
+PRODUCT_PACKAGES += \
+    create_pl_dev \
+    create_pl_dev.recovery
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# OTA postinstall
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 31
+
