642f1fb7e83aa7ef6a1990eeb93d177288921087
Jagath Krishna <91169046+Jagatg@users.noreply.github.com>
2026-05-30 19:53:04 +0530

    ice: Add prebuilt kernel module to Android.mk

--- Android.mk
+++ Android.mk
@@ -4,3 +4,17 @@ ifeq ($(TARGET_DEVICE),ice)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
 
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := kernel
+LOCAL_MODULE_STEM := kernel
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_PREBUILT_MODULE_FILE := $(LOCAL_PATH)-kernel/kernel
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
+
+ifeq ($(TARGET_DEVICE),ice)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
