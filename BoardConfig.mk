954c90dfcc0b35ae59ef2f01185600b1b7ed016b
jagatg <jjja.gath9@gmail.com>
2026-07-06 04:03:39 +0530

    ice: add A/B configs and define arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,28 @@ SELINUX_IGNORE_NEVERALLOWS := true
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 ALLOW_MISSING_DEPENDENCIES := true
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
+# A/B
+AB_OTA_UPDATER := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    product \
+    vendor \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_BOOTLOADER_BOARD_NAME := ice
+TARGET_NO_BOOTLOADER := true
+
