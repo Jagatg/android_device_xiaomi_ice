0cefd7f265fd979540f91d036b61881ad28843d5
jagatg <jjja.gath9@gmail.com>
2026-07-06 13:26:59 +0530

    ice: add everything to device.mk

--- device.mk
+++ device.mk
@@ -1,3 +1,181 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+LOCAL_PATH := device/xiaomi/ice
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# GMS
+ifeq ($(WITH_GMS),true)
+GMS_MAKEFILE=gms_minimal.mk
+endif
+
+# Build hacks
+PRODUCT_ENFORCE_VINTF_MANIFEST := false
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# vendor
+$(call inherit-product, vendor/xiaomi/ice/ice-vendor.mk)
+
+AB_OTA_UPDATER := true
+
+# A/B support
+PRODUCT_PACKAGES += \
+    com.android.hardware.boot \
+    android.hardware.boot-service.default_recovery
+
+PRODUCT_PACKAGES += \
+    create_pl_dev \
+    create_pl_dev.recovery
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# OTA postinstall
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6761:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mt6761
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.product.first_api_level=31
+
+PRODUCT_PACKAGES += \
+    init.recovery.mt6761.rc
+
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/ice/proprietary/vendor/firmware/focaltech_ts_fw_helitai.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/focaltech_ts_fw_helitai.bin \
+    vendor/xiaomi/ice/proprietary/vendor/firmware/novatek_ts_truly_fw.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/novatek_ts_truly_fw.bin \
+    vendor/xiaomi/ice/proprietary/vendor/firmware/novatek_ts_truly_mp.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/novatek_ts_truly_mp.bin
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    libmtk_bsg.recovery
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
+# Manifest
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(LOCAL_PATH)/vintf/compatibility_matrix.xml
+
+# Properties
+TARGET_SYSTEM_PROP += device/xiaomi/ice/configs/prop/system.prop
+TARGET_VENDOR_PROP += device/xiaomi/ice/configs/prop/vendor.prop
+TARGET_PRODUCT_PROP += device/xiaomi/ice/configs/prop/product.prop
+
+# Vendor properties
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.renderengine.backend=gles
+
+PRODUCT_PACKAGES += \
+    fstab.enableswap \
+    fstab.mt6761 \
+    factory_init.connectivity.common.rc \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6761.rc \
+    init.mt6761.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    init.zram.rc \
+    init_connectivity.rc \
+    meta_init.connectivity.common.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    meta_init.vendor.rc \
+    multi_init.rc
+
+PRODUCT_PACKAGES += \
+    IceFrameworksResOverlay \
+    IceLineageSdkOverlay \
+    IceSettingsProviderOverlay \
+    IceSystemUIOverlay \
+    TrebuchetGoOverlay
+
+# Display device config - fixes DisplayDeviceConfig nits spam causing watchdog kill
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/display_port_0.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/displayconfig/display_port_0.xml
+
+PRODUCT_PACKAGES += \
+    Dialer
+
+# enable BesLoudness audio support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/AudioParamOptions.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_param/AudioParamOptions.xml \
+    $(LOCAL_PATH)/audio/AudioParamOptions_vext.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_param/AudioParamOptions_vext.xml
+
+PRODUCT_PACKAGES += \
+    Launcher3QuickStepGo \
+    NoNavigationBarModeGestural
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    fw.max_users=1
+
+# Reduce GC pressure on foreground apps
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.foreground-heap-growth-multiplier=2.0
+
+# Use Go-optimized SystemUI for low-RAM device
+PRODUCT_PACKAGES += \
+    SystemUIGo
+
+DONT_UNCOMPRESS_PRIV_APPS_DEXS := true
+
 # Audio policy configuration
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
