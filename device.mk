3a27dff3025eaea652cd0df3b536fd0bd968f9d3
Your Name <you@example.com>
2026-09-25 21:30:10 +0700

    ice: Build vndservicemanager and vndservice

--- device.mk
+++ device.mk
@@ -100,3 +100,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6761:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6761
 
+PRODUCT_PACKAGES += \
+    dmabuf_dump.vendor \
+    vndservicemanager \
+    vndservice
