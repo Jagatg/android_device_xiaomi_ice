2966116d04834488be5e7da1a09bff5766b7a38d
jagatg <jjjagath9@gmail.com>
2026-09-25 13:50:33 +0530

    ice: Set display resolution and density

--- device.mk
+++ device.mk
@@ -4,6 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/xiaomi/ice
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
