718e6f4df2996dcacdc3bb0120b6d34012913276
jagatg <jjjagath9@gmail.com>
2026-09-25 13:13:31 +0530

    ice: Init tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_ice.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ice-user \
+    lineage_ice-userdebug \
+    lineage_ice-eng
